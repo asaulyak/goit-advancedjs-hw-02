ab2b0396b8d9009ca9753dbbfe09c8c863f511e5
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2024-01-12 19:39:16 +0200

    added color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+function bindEvents() {
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+
+  if (!startButton || !stopButton) {
+    return;
+  }
+
+  let switcherId = null;
+
+  startButton.addEventListener('click', () => {
+    switcherId = startSwitcher();
+    startButton.disabled = true;
+  });
+
+  stopButton.addEventListener('click', () => {
+    stopSwitcher(switcherId);
+    startButton.disabled = false;
+  });
+}
+
+function startSwitcher() {
+  return setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopSwitcher(intervalId) {
+  clearInterval(intervalId);
+}
+
+bindEvents();
